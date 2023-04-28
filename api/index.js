05fed8727ecd4e674a5c36c1667794a0c7e14f48
Facutorress <86375929+Facutorress@users.noreply.github.com>
2023-04-28 09:33:37 -0300

    lol

--- api/index.js
+++ api/index.js
@@ -21,10 +21,10 @@
 require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {PGPORT} = process.env;
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PGPORT, () => {
-    console.log(`s listening at ${PGPORT}`); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
